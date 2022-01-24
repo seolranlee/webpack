04ffa3766c8cf2d85589889597e5cbd63cf8f4d8
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:13:12 +0900

    Development

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     })
   ],
   output: {
