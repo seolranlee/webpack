54fa693ce98cfee8af487470e03a25444f192e97
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:32:34 +0900

    Code Splitting: Prevent Duplication: 2. SplitChunksPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: {
-      import: './src/index.js',
-      dependOn: 'shared',
-    },
-    another: {
-      import: './src/another-module.js',
-      dependOn: 'shared',
-    },
-    shared: 'lodash',
+    index: './src/index.js',
+    another: './src/another-module.js',
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -30,6 +23,8 @@ module.exports = {
     publicPath: '/',
   },
   optimization: {
-    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    }
   }
 }
