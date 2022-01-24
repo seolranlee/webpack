9a47b063031f4d0de13828bbd3a7b26b72f0045c
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 14:56:05 +0900

    Asset Management: Customize parser of JSON modules

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.svg';
 import Data from './data.xml';
-import Notes from './data.csv'
-;
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title);
+console.log(toml.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
+
 function component() {
   const element = document.createElement('div');
 
