674c3cbf5b10f9b8cf607267060efe416fbaa2bc
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:20:15 +0900

    Development: Choosing a Development Tool: 2. Using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     print: './src/print.js',
   },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
