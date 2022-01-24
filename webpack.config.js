e5214bd5ebe547a141beefca4dd67c29d70f5619
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:15:24 +0900

    Development: Using source maps

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
