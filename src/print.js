73981cefd17d7371364cf3899fb084aecf3d4d5e
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:50:23 +0900

    Code Splitting: Dynamic Imports: Lazy Loading

--- src/print.js
+++ src/print.js
@@ -1,3 +1,7 @@
-export default function printMe() {
-  console.log('I get called from print.js auto!');
-}
+console.log(
+  'The print.js module has loaded! See the network tab in dev tools...'
+);
+
+export default () => {
+  console.log('Button Clicked: Here\'s "some text"!');
+};
