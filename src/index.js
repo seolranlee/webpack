4623615c881f45aead014f94e1d6d1488ac2c686
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:05:32 +0900

    Output Management: Preparation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // 이제 Lodash를 스크립트로 가져온다.
-  element.innerHTML = _.join(['Hello', 'webpack', '()=>{}'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
