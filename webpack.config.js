8ffaab14994940ca22f332899e7805d74d4bd792
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:56:12 +0900

    Caching: Output Filenames

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
+  entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Caching',
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
