1cf58e777ce8c378ebf040455d8581474566483d
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:45:45 +0900

    Code Splitting: Dynamic Imports: async, await

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-function getComponent() {
-   const element = document.createElement('div');
+async function getComponent() {
+  const element = document.createElement('div');
+  const { default: _ } = await import('lodash');
 
-  return import('lodash')
-    .then(({ default: _ }) => {
-      const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-      return element;
-    })
-    .catch((error) => 'An error occurred while loading the component');
- }
+  return element;
+}
 
 getComponent().then((component) => {
   document.body.appendChild(component);
