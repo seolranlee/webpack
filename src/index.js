91a58c1c3fe46dccd14fa7f8aa21281e0cddadf5
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 14:14:10 +0900

    Getting Started: Creating a Bundle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   const element = document.createElement('div');
 
-  // 이 라인이 동작하려면 현재 스크립트를 통해 포함된 Lodash가 필요합니다.
+  // 이제 Lodash를 스크립트로 가져왔습니다.
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
