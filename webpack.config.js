05071fba569aeb2a1a5d9e6b68a9602f17e0949c
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-24 15:25:11 +0900

    Code Splitting: Entry Points

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
+    another: './src/another-module.js',
   },
   devtool: 'inline-source-map',
   devServer: {
